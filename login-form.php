52e4a53cad9b644a022daef6b78f28faa93f5a34
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-06-29 17:00:07 +0200

    style on login-form

--- login-form.php
+++ login-form.php
@@ -6,24 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="bear.css">
 </head>
 
 <body>
-    <form action="login-form-handler.php" method="post">
-        <div>
-            <label for="username">Nom d'utilisateur</label>
-            <input type="text" name="username">
+    <div class="flex-center">
+        <div class="container glass">
+            <form action="login-form-handler.php" method="post">
+                <div>
+                    <label for="username">Login : </label><br/>
+                    <input type="text" name="username">
+                </div>
+                <div>
+                    <label for="password" input="password">Password : </label><br/>
+                    <input type="password" name="pwd">
+                </div>
+                <div>
+                    <input type="submit" value="Envoyer">
+                    <a href="home.php"><button>Retour</button></a>
+                </div>
+            </form>
+            
         </div>
-        <div>
-            <label for="password" input="password">Mot de passe</label>
-            <input type="password" name="pwd">
-        </div>
-        <div>
-            <input type="submit" value="Envoyer">
-
-        </div>
-    </form>
-    <a href="home.php"><button>Retour</button></a>
+    </div>
 </body>
 
 </html>
