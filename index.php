1392f3968cb24f8bf9834632c33c7fe5af8ceb54
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-07-01 16:00:01 +0200

    session redirect when user connected

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-if (file_exists("config.php")) {
-    header("Location: home.php");
-}
+session_start(); 
+
+if (isset($_SESSION["username"])) header("Location: dashboard.php");
+else if (file_exists("config.php")) header("Location: home.php");
 else header("Location: bear-setup.php");
