b72f43f1b566d447e7613c92d8aedefdd5fd7157
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-07-01 14:08:24 +0200

    add register

---
+++ register-form-handler.php
@@ -0,0 +1,31 @@
+<?php
+require_once("db-connect.php");
+
+if (
+    isset($_POST['username']) && !empty($_POST['username']) && 
+    isset($_POST['email']) && !empty($_POST['email']) && 
+    isset($_POST['pwd']) && !empty($_POST['pwd']) &&
+    isset($_POST['pwdconf']) && !empty($_POST['pwdconf'])
+    ){
+        if ($_POST['pwd'] === $_POST['pwdconf']) {
+            $username = strip_tags( $_POST['username']);
+            $email = strip_tags($_POST['email']);
+            $password = strip_tags($_POST['pwd']);
+            
+                    $passwordhash = password_hash($password, PASSWORD_DEFAULT);
+
+                    $sql = "INSERT INTO Bear_users(username,email,pwd) VALUES(:username,:email,:password)";
+                    $query = $db->prepare($sql);
+                    $query->bindValue(':username', $username, PDO::PARAM_STR);
+                    $query->bindValue(':email', $email, PDO::PARAM_STR);
+                    $query->bindValue(':password', $passwordhash, PDO::PARAM_STR);
+                    $query->execute();
+                    echo 'Your account has been created';
+                    header("Location: home.php");
+        }
+        else echo 'Passwords not matching';
+            
+}
+else echo 'Every fields must be completed';   
+
+        
---
+++ register-form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BearOffice - Register</title>
+</head>
+
+<body>
+    <form action="register-form-handler.php" method="post">
+        <div>
+            <label for="username">Username: </label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="e-mail">E-mail: </label>
+            <input type="email" name="email">
+        </div>
+        <div>
+            <label for="password" input="password">Password: </label>
+            <input type="password" name="pwd">
+        </div>
+        <div>
+            <label for="password-confirm">Confirm password: </label>
+            <input type="password" name="pwdconf">
+        </div>
+        <div>
+            <input type="submit" value="Submit">
+        </div>
+</body>
+
+</html>
